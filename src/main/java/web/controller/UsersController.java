7c99f13419af3bd7fc344ae43569369260572180
ALiElmurza <ssenioross@gmail.com>
2023-01-09 22:35:36 +0300

    09.01-22:35

--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -1,24 +1,27 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import web.dao.DaoImp;
 import web.models.User;
+import web.services.UserService;
 
 @Controller
 @RequestMapping("/user")
 public class UsersController {
 
-    private final DaoImp daoImp;
+    private final UserService userService;
 
-    public UsersController(DaoImp daoImp) {
-        this.daoImp = daoImp;
+    @Autowired
+    public UsersController(UserService userService) {
+        this.userService = userService;
     }
+
 
     @GetMapping()
     public String getUsers(Model model) {
-        model.addAttribute("users", daoImp.getListUsers());
+        model.addAttribute("users", userService.findAll());
         return "index";
     }
 
@@ -29,49 +32,49 @@ public class UsersController {
 
     @PostMapping()
     public String create(@ModelAttribute("user") User user) {
-        daoImp.save(user);
+        userService.save(user);
         return "redirect:/user";
     }
 
     @GetMapping("/{id}")
     public String getUserById(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("user", daoImp.getUserByID(id));
+        model.addAttribute("user", userService.findOne(id));
         return "show";
     }
 
     @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") Long id) {
-        model.addAttribute("user", daoImp.getUserByID(id));
+        model.addAttribute("user", userService.findOne(id));
         return "edit";
     }
 
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("user") User user) {
-        daoImp.update(user);
+        userService.update(user);
         return "redirect:/user";
     }
 
     @GetMapping("/update")
     public String getListToUpdate(Model model) {
-        model.addAttribute("users", daoImp.getListUsers());
+        model.addAttribute("users", userService.findAll());
         return "update";
     }
 
     @GetMapping("/delete")
     public String getListToDelete(Model model) {
-        model.addAttribute("users", daoImp.getListUsers());
+        model.addAttribute("users", userService.findAll());
         return "delete";
     }
 
     @GetMapping("/{id}/deleteById")
     public String getUserToDelete(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("user", daoImp.getUserByID(id));
+        model.addAttribute("user", userService.findOne(id));
         return "show";
     }
 
     @DeleteMapping("/{id}/delete")
     public String delete(@PathVariable("id") Long id) {
-        daoImp.delete(id);
+        userService.delete(id);
         return "redirect:/user";
     }
 
